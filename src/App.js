5edeca116ff30a11bbbd995f9a25598fbd53052c
youngmentor <olaleresuliton43@gmail.com>
2023-03-11 06:55:14 -0800

    Text to speech

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import { useState } from "react"
 
 function App() {
+  const [ourText, setOurText] = useState("")
+  const msg = new SpeechSynthesisUtterance()
+
+  const speechHandler = (msg) => {
+    msg.text = ourText
+    window.speechSynthesis.speak(msg)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>React Text to Speech App</h1>
+      <textarea
+        rows="10"
+        value={ourText}
+        placeholder='Enter Text'
+        onChange={(e) => setOurText(e.target.value)}
+      ></textarea>
+      <button onClick={() => speechHandler(msg)} className="Speak_Bttn">SPEAK</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+
